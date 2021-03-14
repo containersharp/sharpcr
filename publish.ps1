07dd941fb82cfec5ce2b30e39e38dcfda546d5d8
Jijie Chen <jijie.chen@someplace.com>
2021-03-14 12:21:15 +0800

    Added a script for publishing SharpCR.Registry

---
+++ publish.ps1
@@ -0,0 +1,9 @@
+﻿cd SharpCR.Features.LocalStorage
+dotnet build -c Release -r linux-x64
+
+cd ..
+cd SharpCR.Registry
+dotnet build -c Release -r linux-x64
+dotnet publish --no-build -c Release -r linux-x64
+
+docker build -t "sharpcr-registry:1.0.0" .
