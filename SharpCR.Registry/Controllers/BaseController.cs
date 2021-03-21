167259f5a9428380b40f3ae3568be2f0e9721a80
walle <walle@evy.null>
2021-03-21 18:28:06 +0800

    试验登录认证

--- SharpCR.Registry/Controllers/BaseController.cs
+++ SharpCR.Registry/Controllers/BaseController.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Buffers.Text;
+using System.Linq;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SharpCR.Registry.Controllers
@@ -8,12 +12,45 @@ namespace SharpCR.Registry.Controllers
         [Route("")]
         public ActionResult Base()
         {
-            HttpContext.Response.Headers.Add("Docker-Distribution-API-Version", "registry/2.0");
+            HttpContext.Response.Headers.Add(
+                "Docker-Distribution-API-Version",
+                "registry/2.0");
             HttpContext.Response.Headers.Add("Vary", "Authorization");
+
+            var token = HttpContext.Request.Headers["Authorization"].ToString();
+
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                HttpContext.Response.Headers.Add(
+                    "Www-Authenticate",
+                    "Bearer realm=\"http://kubernetes.docker.internal:5000/v2/token\"");
+                return new UnauthorizedResult();
+            }
+
+            Console.WriteLine("token: " + token);
 
             return new OkResult();
         }
-        
+
+        [Route("token")]
+        public ActionResult Token()
+        {
+            var auth = HttpContext.Request.Headers["Authorization"].ToString();
+            var strs = Encoding.UTF8.GetString(
+                Convert
+                    .FromBase64String(auth.Remove(0, "Basic ".Length))).Split(":");
+
+            Console.WriteLine(
+                "username: " + strs[0] + ", password: " + strs[1]);
+
+            return new ObjectResult(
+                new
+                {
+                    access_token = "eyJhbGciOiJFUzI1NiIsInR5",
+                }
+            );
+        }
+
         [Route("_catalog")]
         public object Catalog()
         {
