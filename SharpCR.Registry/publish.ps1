d01579939b294d1593467be90208c68537c897ee
Jijie Chen <jijie.chen@someplace.com>
2021-04-03 20:21:08 +0800

    Added publish script enabling specifing version when running publish command lines

---
+++ SharpCR.Registry/publish.ps1
@@ -0,0 +1,18 @@
+﻿$VER=$args[0] # || $1
+
+if(-not $VER){
+    $VER='dev'    
+}
+
+dotnet restore --runtime linux-x64
+
+cd ..
+cd SharpCR.Features.LocalStorage
+dotnet build -c Release -r linux-x64
+
+cd ..
+cd SharpCR.Registry
+dotnet build -c Release -r linux-x64
+dotnet publish -c Release --no-build -r linux-x64
+
+docker build -t "jijiechen-docker.pkg.coding.net/sharpcr/apps/sharpcr-registry:$VER" .
---
+++ SharpCR/publish.ps1
@@ -0,0 +1,11 @@
+﻿$VER=$args[0] # || $1
+
+if(-not $VER){
+    $VER='1.0.0-dev'    
+}
+
+dotnet publish -c Release -r linux-x64
+
+dotnet pack -c Release --runtime linux-x64 /p:VersionPrefix=$VER
+
+dotnet nuget push --source integration --api-key api "bin\Release\SharpCR.$VER.nupkg"
